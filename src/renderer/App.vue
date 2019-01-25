1bf323416f20a3ce5f8e4968eac87642d2a8f3eb
daizq <502941254@qq.com>
2019-01-25 10:37:42 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hanas'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
